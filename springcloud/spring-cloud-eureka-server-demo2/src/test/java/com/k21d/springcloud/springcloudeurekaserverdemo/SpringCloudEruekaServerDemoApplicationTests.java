001075239efc68854abaea2e591607b972f19657
519118981 <519118981@qq.com>
2019-11-18 23:53:20 +0800

    Eureka Server Demo

---
+++ springcloud/spring-cloud-eureka-server-demo2/src/test/java/com/k21d/springcloud/springcloudeurekaserverdemo/SpringCloudEruekaServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k21d.springcloud.springcloudeurekaserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEruekaServerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
